b4373374641914d9967ee81d80d2214b018cd499
Carlos Souza <cadusouza2001@gmail.com>
2024-10-14 00:19:06 -0300

    Implement section based target system

--- bin/main.cpp
+++ bin/main.cpp
@@ -33,8 +33,8 @@ using namespace std;
 using namespace glm;
 
 GoalLimits goalLimits = {
-	{209.0f, 414.0f}, // leftBottom
-	{591.0f, 550.0f}  // rightTop
+    {209.0f, 414.0f}, // leftBottom
+    {591.0f, 550.0f}  // rightTop
 };
 
 // Protótipo da função de callback de teclado
@@ -42,8 +42,6 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 
 // Dimensões da janela (pode ser alterado em tempo de execução)
 const GLuint WIDTH = 800, HEIGHT = 600;
-
-float vel = 1.2;
 
 bool keys[1024] = {false};
 
@@ -67,7 +65,8 @@ bool isBallAnimationComplete = false;
 bool wasSpacePressed = false;
 bool isGoalkeeperAnimationComplete = false;
 
-Coordinates kickTarget = {0.0f, 0.0f};
+Coordinates selectedKickTarget = {0.0f, 0.0f};
+Coordinates calculatedKickTarget = {0.0f, 0.0f};
 
 bool showTarget = false;
 float circleDisplayTime = 360.0f;
@@ -79,109 +78,172 @@ bool playerDefended = false;
 
 bool mouseClickedOn(GLFWwindow *window, vec3 targetPos)
 {
-	double mouseX, mouseY;
-	glfwGetCursorPos(window, &mouseX, &mouseY);
+    double mouseX, mouseY;
+    glfwGetCursorPos(window, &mouseX, &mouseY);
 
-	float dx = mouseX - targetPos.x;
-	float dy = HEIGHT - mouseY - targetPos.y;
+    float dx = mouseX - targetPos.x;
+    float dy = HEIGHT - mouseY - targetPos.y;
 
-	return sqrt(dx * dx + dy * dy) < 50.0f; // Raio de tolerância para defesa
+    return sqrt(dx * dx + dy * dy) < 50.0f; // Raio de tolerância para defesa
 }
 
 void mouseButtonCallback(GLFWwindow *window, int button, int action, int mods)
 {
-	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
-	{
-		double xpos, ypos;
-		glfwGetCursorPos(window, &xpos, &ypos);
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
 
-		// Get the window height to invert the y-coordinate
-		int windowWidth, windowHeight;
-		glfwGetWindowSize(window, &windowWidth, &windowHeight);
-		ypos = windowHeight - ypos;
+        // Get the window height to invert the y-coordinate
+        int windowWidth, windowHeight;
+        glfwGetWindowSize(window, &windowWidth, &windowHeight);
+        ypos = windowHeight - ypos;
 
-		std::cout << "Mouse clicked at: (" << xpos << ", " << ypos << ")" << std::endl;
-	}
+        std::cout << "Mouse clicked at: (" << xpos << ", " << ypos << ")" << std::endl;
+    }
 }
 
 // Function to set up the mouse click callback
 void setupMouseClickLogging(GLFWwindow *window)
 {
-	glfwSetMouseButtonCallback(window, mouseButtonCallback);
+    glfwSetMouseButtonCallback(window, mouseButtonCallback);
 }
 
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
 
-	if (action == GLFW_PRESS)
-		keys[key] = true;
+    if (action == GLFW_PRESS)
+        keys[key] = true;
 
-	if (action == GLFW_RELEASE)
-		keys[key] = false;
+    if (action == GLFW_RELEASE)
+        keys[key] = false;
 }
 
 void resetPositions()
 {
-	ball.resetPositions();
-	player.resetPositions();
-	goalkeeper.resetPositions();
-	isPlayerShooting = false;
-	isPlayerSelectingTarget = false;
-	isVerticalArrowMoving = false;
-	isKickAnimationComplete = false;
-	isBallAnimationComplete = false;
-	showTarget = false;
-	circleTimer = 0.0f;
+    ball.resetPositions();
+    player.resetPositions();
+    goalkeeper.resetPositions();
+    isPlayerShooting = false;
+    isPlayerSelectingTarget = false;
+    isVerticalArrowMoving = false;
+    isKickAnimationComplete = false;
+    isBallAnimationComplete = false;
+    showTarget = false;
+    circleTimer = 0.0f;
+}
+
+void determineTarget()
+{
+    if (selectedKickTarget.x < goalLimits.leftBottom.x || selectedKickTarget.x > goalLimits.rightTop.x ||
+        selectedKickTarget.y < goalLimits.leftBottom.y || selectedKickTarget.y > goalLimits.rightTop.y)
+    {
+        // Out of bounds, set to default target
+        calculatedKickTarget.x = 1000.0f;
+        calculatedKickTarget.y = 1000.0f;
+    }
+    else
+    {
+
+        float thirdX = (goalLimits.rightTop.x - goalLimits.leftBottom.x) / 3.0f;
+        float midY = (goalLimits.leftBottom.y + goalLimits.rightTop.y) / 2.0f;
+
+        if (selectedKickTarget.x < goalLimits.leftBottom.x + thirdX)
+        {
+            if (selectedKickTarget.y < midY)
+            {
+                // Bottom left corner
+                calculatedKickTarget.x = goalLimits.leftBottom.x + thirdX / 2.0f;
+                calculatedKickTarget.y = goalLimits.leftBottom.y + (midY - goalLimits.leftBottom.y) / 2.0f;
+            }
+            else
+            {
+                // Top left corner
+                calculatedKickTarget.x = goalLimits.leftBottom.x + thirdX / 2.0f;
+                calculatedKickTarget.y = goalLimits.rightTop.y - (goalLimits.rightTop.y - midY) / 2.0f;
+            }
+        }
+        else if (selectedKickTarget.x < goalLimits.leftBottom.x + 2 * thirdX)
+        {
+            if (selectedKickTarget.y < midY)
+            {
+                // Bottom middle
+                calculatedKickTarget.x = goalLimits.leftBottom.x + thirdX + thirdX / 2.0f;
+                calculatedKickTarget.y = goalLimits.leftBottom.y + (midY - goalLimits.leftBottom.y) / 2.0f;
+            }
+            else
+            {
+                // Top middle
+                calculatedKickTarget.x = goalLimits.leftBottom.x + thirdX + thirdX / 2.0f;
+                calculatedKickTarget.y = goalLimits.rightTop.y - (goalLimits.rightTop.y - midY) / 2.0f;
+            }
+        }
+        else
+        {
+            if (selectedKickTarget.y < midY)
+            {
+                // Bottom right corner
+                calculatedKickTarget.x = goalLimits.leftBottom.x + 2 * thirdX + thirdX / 2.0f;
+                calculatedKickTarget.y = goalLimits.leftBottom.y + (midY - goalLimits.leftBottom.y) / 2.0f;
+            }
+            else
+            {
+                // Top right corner
+                calculatedKickTarget.x = goalLimits.leftBottom.x + 2 * thirdX + thirdX / 2.0f;
+                calculatedKickTarget.y = goalLimits.rightTop.y - (goalLimits.rightTop.y - midY) / 2.0f;
+            }
+        }
+    }
 }
 
 int main()
 {
-	glfwInit();
-	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Penalty Fever", nullptr, nullptr);
-	glfwMakeContextCurrent(window);
-	// Fazendo o registro da função de callback para a janela GLFW
-	glfwSetKeyCallback(window, key_callback);
-	// GLAD: carrega todos os ponteiros d funções da OpenGL
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-	}
-	// Obtendo as informações de versão
-	const GLubyte *renderer = glGetString(GL_RENDERER); /* get renderer string */
-	const GLubyte *version = glGetString(GL_VERSION);	/* version as a string */
-	cout << "Renderer: " << renderer << endl;
-	cout << "OpenGL version supported " << version << endl;
-	// Definindo as dimensões da viewport com as mesmas dimensões da janela da aplicação
-	int width, height;
-	glfwGetFramebufferSize(window, &width, &height);
-	glViewport(0, 0, width, height);
-	// Compilando e buildando o programa de shader
-	// Gerando um buffer simples, com a geometria de um triângulo
+    glfwInit();
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Penalty Fever", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+    // Fazendo o registro da função de callback para a janela GLFW
+    glfwSetKeyCallback(window, key_callback);
+    // GLAD: carrega todos os ponteiros d funções da OpenGL
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+    }
+    // Obtendo as informações de versão
+    const GLubyte *renderer = glGetString(GL_RENDERER); /* get renderer string */
+    const GLubyte *version = glGetString(GL_VERSION);   /* version as a string */
+    cout << "Renderer: " << renderer << endl;
+    cout << "OpenGL version supported " << version << endl;
+    // Definindo as dimensões da viewport com as mesmas dimensões da janela da aplicação
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    // Compilando e buildando o programa de shader
+    // Gerando um buffer simples, com a geometria de um triângulo
 
-	background.setupSprite();
-	player.setupSprite();
-	ball.setupSprite();
-	goalkeeper.setupSprite();
-	horizontalArrow.setupSprite();
-	verticalArrow.setupSprite();
-	target.setupSprite();
+    background.setupSprite();
+    player.setupSprite();
+    ball.setupSprite();
+    goalkeeper.setupSprite();
+    horizontalArrow.setupSprite();
+    verticalArrow.setupSprite();
+    target.setupSprite();
 
-	shaderID = setupShader();
-	glUseProgram(shaderID);
-	glUniform1i(glGetUniformLocation(shaderID, "texBuffer"), 0);
-	mat4 projection = ortho(0.0f, 800.0f, 0.0f, 600.0f, -1.0f, 1.0f);
-	glUniformMatrix4fv(glGetUniformLocation(shaderID, "projection"), 1, GL_FALSE, value_ptr(projection));
-	glActiveTexture(GL_TEXTURE0);
-	glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glEnable(GL_DEPTH_TEST);
-	// Set up mouse click logging
-	setupMouseClickLogging(window);
-	glDepthFunc(GL_ALWAYS);
+    shaderID = setupShader();
+    glUseProgram(shaderID);
+    glUniform1i(glGetUniformLocation(shaderID, "texBuffer"), 0);
+    mat4 projection = ortho(0.0f, 800.0f, 0.0f, 600.0f, -1.0f, 1.0f);
+    glUniformMatrix4fv(glGetUniformLocation(shaderID, "projection"), 1, GL_FALSE, value_ptr(projection));
+    glActiveTexture(GL_TEXTURE0);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_DEPTH_TEST);
+    // Set up mouse click logging
+    setupMouseClickLogging(window);
+    glDepthFunc(GL_ALWAYS);
 
-	while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Background color
@@ -212,16 +274,17 @@ int main()
                 {
                     if (!isVerticalArrowMoving)
                     {
-                        kickTarget.x = horizontalArrow.sprite.position.x;
+                        selectedKickTarget.x = horizontalArrow.sprite.position.x;
                         isVerticalArrowMoving = true;
                     }
                     else
                     {
-                        kickTarget.y = verticalArrow.sprite.position.y;
+                        selectedKickTarget.y = verticalArrow.sprite.position.y;
                         ball.sprite.lastTime = glfwGetTime();
                         isPlayerSelectingTarget = false;
-                        ball.totalBallDistance = sqrt(pow(kickTarget.x - ball.sprite.position.x, 2) +
-                                                      pow(kickTarget.y - ball.sprite.position.y, 2));
+                        determineTarget();
+                        ball.totalBallDistance = sqrt(pow(calculatedKickTarget.x - ball.sprite.position.x, 2) +
+                                                      pow(calculatedKickTarget.y - ball.sprite.position.y, 2));
                     }
                     wasSpacePressed = true;
                 }
@@ -234,7 +297,7 @@ int main()
             {
                 if (!showTarget)
                 {
-                    target.selectExactKick(kickTarget, goalLimits);
+                    target.selectExactKick(calculatedKickTarget, goalLimits);
                     showTarget = true;
                 }
 
@@ -256,7 +319,7 @@ int main()
                         {
                             if (!isBallAnimationComplete)
                             {
-                                ball.moveBall(isBallAnimationComplete, kickTarget);
+                                ball.moveBall(isBallAnimationComplete, calculatedKickTarget);
                             }
                             else
                             {
